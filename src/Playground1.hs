dbd4dc837d21543c0ce9fb085cc84f865c8febe7
Diogo Castro <dc@diogocastro.com>
2026-04-19 23:26:01 +0100

    Mixing linear IO with system IO

--- src/Playground1.hs
+++ src/Playground1.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE GHC2024 #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE LinearTypes #-}
 {-# LANGUAGE OverloadedRecordDot #-}
@@ -10,11 +9,31 @@
 
 module Playground1 where
 
-import Control.Functor.Linear qualified as L
+import Control.Functor.Linear as L
 import Prelude.Linear hiding (IO)
 import System.IO.Linear (IO)
+import System.IO.Linear qualified as IO
 
 t2 :: IO Int
 t2 = L.do
   x <- L.pure 1
   L.pure x
+
+lockScope :: (Int %1 -> IO Int) -> IO Int
+lockScope run = L.do
+  run 1
+
+lock ::
+  Int %1 ->
+  IO Int
+lock x = do
+  pure x
+
+f2 :: IO Int
+f2 = L.do
+  lockScope \x -> L.do
+    x2 <- lock x
+    IO.fromSystemIO $ putStrLn "aa"
+    Ur line <- IO.fromSystemIOU $ getLine
+    x2 <- lock x2
+    pure x2
