3c0be8d0179c2f8ab179ea7895e74e749ab1f704
Diogo Castro <dc@diogocastro.com>
2026-04-20 15:44:38 +0100

    More experiments

--- src/Playground1.hs
+++ src/Playground1.hs
@@ -13,6 +13,7 @@ import Control.Functor.Linear as L
 import Prelude.Linear hiding (IO)
 import System.IO.Linear (IO)
 import System.IO.Linear qualified as IO
+import Prelude qualified as P
 
 t2 :: IO Int
 t2 = L.do
@@ -31,11 +32,39 @@ lock ::
 lock x = do
   pure x
 
+lock' ::
+  Int %1 ->
+  P.IO Int
+lock' x = do
+  case move x of
+    Ur x' -> P.pure x'
+
 f2 :: IO String
 f2 = L.do
-  lockScope \x -> L.do
-    x2 <- lock x
+  lockScope \key -> L.do
+    key2 <- lock key
     IO.fromSystemIO $ putStrLn "aa"
-    Ur line <- IO.fromSystemIOU $ getLine
-    x2 <- lock x2
-    pure (x2, line)
+    -- This doesn't compile, `print` is not linear.
+    -- IO.fromSystemIO $ print key2
+
+    -- Non-linear IO with unrestricted value
+    Ur line <- IO.fromSystemIOU getLine
+    -- Non-linear IO with linear value
+    line <- IO.fromSystemIO getLine
+    -- Consume linear variable in non-linear IO
+    line2 <- IO.fromSystemIO getLine
+    IO.fromSystemIO $ putStrLnLinear line2
+
+    Ur () <- IO.fromSystemIOU $ IO.withLinearIO do
+      pure $ move ()
+
+    -- Use key in non-linear IO
+    key3 <- IO.fromSystemIO $ lock' key2
+
+    key4 <- lock key3
+    pure (key4, line)
+
+putStrLnLinear :: String %1 -> P.IO ()
+putStrLnLinear str =
+  case move str of
+    Ur str' -> putStrLn str'
