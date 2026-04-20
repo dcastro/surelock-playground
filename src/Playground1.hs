571ccbe9dbd0b8e96b30ee5f9afb41d753e87cd3
Diogo Castro <dc@diogocastro.com>
2026-04-20 16:00:34 +0100

    Switched to base Prelude

--- src/Playground1.hs
+++ src/Playground1.hs
@@ -3,68 +3,66 @@
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE QualifiedDo #-}
 {-# LANGUAGE NoFieldSelectors #-}
-{-# LANGUAGE NoImplicitPrelude #-}
 
 -- NOTE: we must NOT enable `ApplicativeDo`
 
 module Playground1 where
 
-import Control.Functor.Linear as L
-import Prelude.Linear hiding (IO)
-import System.IO.Linear (IO)
-import System.IO.Linear qualified as IO
-import Prelude qualified as P
+import Control.Functor.Linear qualified as L
+import Prelude.Linear (Ur (..), move)
+import Prelude.Linear qualified as L hiding (IO)
+import System.IO.Linear qualified as L
 
-t2 :: IO Int
+t2 :: L.IO Int
 t2 = L.do
   x <- L.pure 1
   L.pure x
 
-lockScope :: (Int %1 -> IO (Int, a)) -> IO a
+lockScope :: (Int %1 -> L.IO (Int, a)) -> L.IO a
 lockScope run = L.do
   (i, a) <- run 1
-  case move i of Ur _ -> pure ()
-  pure a
+  case move i of Ur _ -> L.pure ()
+  L.pure a
 
 lock ::
   Int %1 ->
-  IO Int
+  L.IO Int
 lock x = do
-  pure x
+  L.pure x
 
 lock' ::
   Int %1 ->
-  P.IO Int
+  IO Int
 lock' x = do
   case move x of
-    Ur x' -> P.pure x'
+    Ur x' -> pure x'
 
-f2 :: IO String
-f2 = L.do
+f2 :: L.IO String
+f2 = do
   lockScope \key -> L.do
     key2 <- lock key
-    IO.fromSystemIO $ putStrLn "aa"
+    L.fromSystemIO (putStrLn "aa")
     -- This doesn't compile, `print` is not linear.
     -- IO.fromSystemIO $ print key2
 
     -- Non-linear IO with unrestricted value
-    Ur line <- IO.fromSystemIOU getLine
+    Ur line <- L.fromSystemIOU getLine
     -- Non-linear IO with linear value
-    line <- IO.fromSystemIO getLine
+    line <- L.fromSystemIO getLine
     -- Consume linear variable in non-linear IO
-    line2 <- IO.fromSystemIO getLine
-    IO.fromSystemIO $ putStrLnLinear line2
+    line2 <- L.fromSystemIO getLine
+    L.fromSystemIO (putStrLnLinear line2)
 
-    Ur () <- IO.fromSystemIOU $ IO.withLinearIO do
-      pure $ move ()
+    Ur () <- L.fromSystemIOU L.$ L.withLinearIO do
+      L.pure L.$ move ()
 
     -- Use key in non-linear IO
-    key3 <- IO.fromSystemIO $ lock' key2
+    key3 <- L.fromSystemIO L.$ lock' key2
 
     key4 <- lock key3
-    pure (key4, line)
+    L.pure (key4, line)
 
-putStrLnLinear :: String %1 -> P.IO ()
+putStrLnLinear :: String %1 -> IO ()
 putStrLnLinear str =
   case move str of
     Ur str' -> putStrLn str'
